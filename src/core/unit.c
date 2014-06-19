7cc22601d8f5521ed0d168bad0616f09aa95fd2d
The Initfinder General <boycottsystemd@openmailbox.org>
2014-06-19 13:33:21 -0400

    unit: address device unit vtable entry and unit_add_node_link() function

--- src/core/unit.c
+++ src/core/unit.c
@@ -54,7 +54,6 @@ const UnitVTable * const unit_vtable[_UNIT_TYPE_MAX] = {
         [UNIT_TIMER] = &timer_vtable,
         [UNIT_SOCKET] = &socket_vtable,
         [UNIT_TARGET] = &target_vtable,
-        [UNIT_DEVICE] = &device_vtable,
         [UNIT_MOUNT] = &mount_vtable,
         [UNIT_AUTOMOUNT] = &automount_vtable,
         [UNIT_SNAPSHOT] = &snapshot_vtable,
@@ -2348,43 +2347,6 @@ int unit_deserialize(Unit *u, FILE *f, FDSet *fds) {
                 if (r < 0)
                         return r;
         }
-}
-
-int unit_add_node_link(Unit *u, const char *what, bool wants) {
-        Unit *device;
-        _cleanup_free_ char *e = NULL;
-        int r;
-
-        assert(u);
-
-        if (!what)
-                return 0;
-
-        /* Adds in links to the device node that this unit is based on */
-
-        if (!is_device_path(what))
-                return 0;
-
-        e = unit_name_from_path(what, ".device");
-        if (!e)
-                return -ENOMEM;
-
-        r = manager_load_unit(u->manager, e, NULL, NULL, &device);
-
-        if (r < 0)
-                return r;
-
-        r = unit_add_two_dependencies(u, UNIT_AFTER, UNIT_BINDS_TO, device, true);
-        if (r < 0)
-                return r;
-
-        if (wants) {
-                r = unit_add_dependency(device, UNIT_WANTS, u, false);
-                if (r < 0)
-                        return r;
-        }
-
-        return 0;
 }
 
 int unit_coldplug(Unit *u) {
