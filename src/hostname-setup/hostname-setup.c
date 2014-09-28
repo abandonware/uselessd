0c5219dfd57089c5c36eb4ec2538f1273a076e85
The Initfinder General <boycottsystemd@openmailbox.org>
2014-09-28 07:15:50 -0400

    move out hostname-setup into its own tool

--- src/hostname-setup/hostname-setup.c
+++ src/hostname-setup/hostname-setup.c
@@ -25,11 +25,12 @@
 #include <string.h>
 #include <stdlib.h>
 
-#include "hostname-setup.h"
 #include "macro.h"
 #include "util.h"
 #include "log.h"
 #include "fileio.h"
+
+int hostname_setup(void);
 
 static int read_and_strip_hostname(const char *path, char **hn) {
         char *s;
@@ -89,4 +90,75 @@ int hostname_setup(void) {
 
         log_info("Set hostname to <%s>.", hn);
         return 0;
+}
+
+static int help(void) {
+
+        printf("%s [OPTIONS...]\n\n"
+               "Initialize and set /etc/hostname.\n\n"
+               "  -h --help             Show this help\n"
+               "     --version          Show package version\n",
+               program_invocation_short_name);
+
+        return 0;
+}
+
+static int parse_argv(int argc, char *argv[]) {
+
+        enum {
+                ARG_VERSION = 0x100
+        };
+
+        static const struct option options[] = {
+                { "help",      no_argument,       NULL, 'h'           },
+                { "version",   no_argument,       NULL, ARG_VERSION   },
+                { NULL,        0,                 NULL, 0             }
+        };
+
+        int c;
+
+        assert(argc >= 0);
+        assert(argv);
+
+        while ((c = getopt_long(argc, argv, "hqcv", options, NULL)) >= 0) {
+
+                switch (c) {
+
+                case 'h':
+                        help();
+                        return 0;
+
+                case ARG_VERSION:
+                        puts(PACKAGE_STRING);
+                        puts(SYSTEMD_FEATURES);
+                        return 0;
+
+                case '?':
+                        return -EINVAL;
+
+                default:
+                        log_error("Unknown option code %c", c);
+                        return -EINVAL;
+                }
+        }
+
+        if (optind < argc) {
+                help();
+                return -EINVAL;
+        }
+
+        return 1;
+}
+
+int main(void) {
+	   int r;
+
+	   log_parse_environment();
+	   log_open();
+
+	   r = parse_argv(argc, argv);
+       if (r <= 0)
+               return r < 0 ? EXIT_FAILURE : EXIT_SUCCESS;
+
+	   return hostname_setup() < 0 ? EXIT_FAILURE : EXIT_SUCCESS;
 }
