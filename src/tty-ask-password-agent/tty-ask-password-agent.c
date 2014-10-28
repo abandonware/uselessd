3a937b6d2327bbf4b82dec264b8b8383d77e8d2f
The Initfinder General <boycottsystemd@openmailbox.org>
2014-10-28 09:02:23 -0400

    tty-ask-password-agent: conditionalize Plymouth querying

--- src/tty-ask-password-agent/tty-ask-password-agent.c
+++ src/tty-ask-password-agent/tty-ask-password-agent.c
@@ -58,7 +58,7 @@ static int ask_password_plymouth(
                 const char *flag_file,
                 bool accept_cached,
                 char ***_passphrases) {
-
+#ifdef ENABLE_PLYMOUTH
         int fd = -1, notify = -1;
         union sockaddr_union sa = {};
         char *packet = NULL;
@@ -243,6 +243,9 @@ finish:
         free(packet);
 
         return r;
+#else
+        return 0;
+#endif
 }
 
 static int parse_password(const char *filename, char **wall) {
@@ -704,8 +707,12 @@ static int parse_argv(int argc, char *argv[]) {
                         break;
 
                 case ARG_PLYMOUTH:
+                #ifdef ENABLE_PLYMOUTH
                         arg_plymouth = true;
-                        break;
+                #else
+                        log_error("We've been built without Plymouth support.");
+                        return 0;
+                #endif
 
                 case ARG_CONSOLE:
                         arg_console = true;
