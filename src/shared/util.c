5012049a1b41a16d9f85e277ad413567a9678747
The Initfinder General <boycottsystemd@openmailbox.org>
2014-11-07 15:12:03 -0500

    parse_boolean(): cleaner formatting

--- src/shared/util.c
+++ src/shared/util.c
@@ -205,10 +205,23 @@ int unlink_noerrno(const char *path) {
 int parse_boolean(const char *v) {
         assert(v);
 
-        if (streq(v, "1") || v[0] == 'y' || v[0] == 'Y' || v[0] == 't' || v[0] == 'T' || strcaseeq(v, "on"))
-                return 1;
-        else if (streq(v, "0") || v[0] == 'n' || v[0] == 'N' || v[0] == 'f' || v[0] == 'F' || strcaseeq(v, "off"))
-                return 0;
+        if (streq(v, "1")
+           || v[0] == 'y'
+           || v[0] == 'Y'
+           || v[0] == 't'
+           || v[0] == 'T'
+           || strcaseeq(v, "on"))
+
+                     return 1;
+
+        else if (streq(v, "0")
+           || v[0] == 'n'
+           || v[0] == 'N'
+           || v[0] == 'f'
+           || v[0] == 'F'
+           || strcaseeq(v, "off"))
+
+                     return 0;
 
         return -EINVAL;
 }
